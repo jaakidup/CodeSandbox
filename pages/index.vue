901f0625baa7d9d95f44fe75ae4bc7ad4939c65a
Jaaki <jaakidup@gmail.com>
2019-04-30 21:35:58 +0200

    Initial commit

---
+++ pages/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <section>
+    <h1>Hello</h1>
+    <br>
+    {{first}}
+  </section>
+</template>
+
+<script>
+const something = ["Yay", "cool"];
+
+export default {
+  data() {
+    return {
+      first: something
+    };
+  }
+};
+</script>
+
+<style scoped>
+</style>
